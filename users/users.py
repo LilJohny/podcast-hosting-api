80b7d5d9227604cee3cadba5d2f53ab8d1523fbe
Denis Ivanenko <ivanenko@ucu.edu.ua>
2022-04-05 23:32:26 +0300

    Initial commit

---
+++ users/users.py
@@ -0,0 +1,14 @@
+from fastapi_users import FastAPIUsers
+
+from users.authentication.auth_backend import auth_backend
+from users.models import UserDB, UserUpdate, UserCreate, User
+from users.user_manager import get_user_manager
+
+fastapi_users = FastAPIUsers(
+    get_user_manager,
+    [auth_backend],
+    User,
+    UserCreate,
+    UserUpdate,
+    UserDB,
+)
