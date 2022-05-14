376d7dc84e33c01bc0a335b240ea1c7946b1e2df
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-14 18:46:55 +0300

    A lot of updates to show models, removed on startup tables creation

--- main.py
+++ main.py
@@ -39,11 +39,6 @@ async def authenticated_route(user: UserDB = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
 
 
-@app.on_event("startup")
-async def on_startup():
-    # Not needed if you setup a migration system like Alembic
-    await create_db_and_tables()
-    await init_db()
 
 @app.get("/url-list")
 def get_all_urls():
--- shows/models.py
+++ shows/models.py
@@ -1,25 +1,53 @@
 import datetime
+import uuid as uuid_lib
+from pydantic import AnyUrl, Json, FileUrl
+from sqlmodel import SQLModel, Field, Enum, Column
+import enum
 
-from pydantic import AnyUrl, Json
-from sqlmodel import SQLModel, Field
+
+class Language(str, enum.Enum):
+    english = "en"
+
+
+class Category(str, enum.Enum):
+    arts_books = "Arts/Books"
+
+
+class ImageBase(SQLModel):
+    file_url: FileUrl
+    title: str
+    show_link: AnyUrl = Field(default=None, foreign_key="show.show_link")
 
 
 class ShowBase(SQLModel):
     title: str
-    podcast_link: AnyUrl
+    show_link: AnyUrl
     media_link: AnyUrl
     description: str
     generator: str
-    language: str
+    language: Language = Field(sa_column=Column(Enum(Language)))
     show_copyright: str
     last_build_date: datetime.datetime
-    image: str
+    image: ImageBase
     podcast_owner: str
     is_locked: bool
-    category: str
+    category: Category = Field(sa_column=Column(Enum(Category)))
+
+
+class Image(ImageBase, table=True):
+    id: uuid_lib.UUID = Field(
+        default_factory=uuid_lib.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
 
 
 class Show(ShowBase, table=True):
-    id:int = Field(default=None, primary_key=True)
-
-
+    id: uuid_lib.UUID = Field(
+        default_factory=uuid_lib.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+    image: uuid_lib.UUID = Field(default=None, foreign_key="image.id")
