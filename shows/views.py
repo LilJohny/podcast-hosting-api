01c4cbf1f49c3b4b14482155b37d9382c2eafc2f
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-16 22:57:42 +0300

    Added shows CRUDL

--- shows/views.py
+++ shows/views.py
@@ -1,28 +1,41 @@
 import uuid
 from fastapi import status, APIRouter
-from shows.models import ShowBase
+
+from db import get_entity, save_entity
+from shows.models import ShowBase, Show
 
 shows_router = APIRouter(prefix="/shows")
 
 
 @shows_router.post("/create", status_code=status.HTTP_201_CREATED)
-def create_show(show:ShowBase):
-    pass
+async def create_show(show_param: ShowBase) -> ShowBase:
+    show = Show(**show_param.dict(), is_removed=False, id=str(uuid.uuid4()))
+    await save_entity(show)
+    return show
 
 
 @shows_router.delete("/{show_id}")
-def delete_show(show_id: uuid.UUID):
-    pass
+async def delete_show(show_id: str):
+    show = await get_entity(show_id, Show)
+    show.is_removed = True
+    await save_entity(show)
+    return status.HTTP_202_ACCEPTED
 
 
 @shows_router.put("/{show_id}")
-def update_show(show_id: uuid.UUID):
-    pass
+async def update_show(show_id: str, show_param: ShowBase) -> Show:
+    show = await get_entity(show_id, Show)
+    show_data = show.dict()
+    show_data.update(show_param.dict())
+    show = Show(**show_data)
+    await save_entity(show)
+    return show
 
 
 @shows_router.get("/{show_id}")
-def read_show(show_id: uuid.UUID):
-    pass
+async def read_show(show_id: str) -> ShowBase:
+    image = await get_entity(show_id, Show)
+    return ShowBase(**image.dict())
 
 
 @shows_router.get("/")
