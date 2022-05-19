1645d4ee7fd8b0d4c215274192201794d6d21cc1
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-20 00:25:50 +0300

    Added show foreign key and series field

--- episodes/models.py
+++ episodes/models.py
@@ -1,5 +1,6 @@
 import datetime
 import enum
+import uuid as uuid_lib
 
 from pydantic import HttpUrl
 from sqlmodel import SQLModel, Field, Enum, Column
@@ -24,6 +25,8 @@ class EpisodeDTO(SQLModel):
     pub_date: datetime.datetime
     duration: int
     episode_type: EpisodeType = Field(sa_column=Column(Enum(EpisodeType)))
+    show_id: uuid_lib.UUID = Field(default=None, foreign_key="show.id")
+    series: str = Field(default=None, nullable=True)
 
 
 class Episode(EpisodeDTO, UUIDModel, DeletableModel, table=True):
