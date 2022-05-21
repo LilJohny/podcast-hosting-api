76efb1be9ab644090548ecf7d4422baa798e149d
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-21 20:48:03 +0300

    Refactoring

--- images/views.py
+++ images/views.py
@@ -13,7 +13,6 @@ images_router = APIRouter(prefix="/images")
 
 @images_router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_image(image_title: str, image_file: UploadFile = File(...)) -> Image:
-    _, ext = os.path.splitext(image_file.filename)
     s3_key = get_s3_key(image_file.filename, image_title)
     image_url = await upload_file_to_s3(s3_key, image_file.file, FileKind.IMAGE)
     image = Image(title=image_title, file_url=image_url, is_removed=False)
