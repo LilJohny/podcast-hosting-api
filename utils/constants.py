d92a8fc37ca3baaab3cf829659a82fb440bd866b
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-22 23:11:07 +0300

    Updated forgot password template

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@ forgot_password_mail_template = {
     "url": "/messages",
     "method": "POST",
     "data": {
-        "from": "Mailgun Sandbox <postmaster@sandboxf9ad1d859bcc4c67bfaeb67f6f49d2a5.mailgun.org>",
+        "from": "Hermes <hermes@amphora.digital>",
         "subject": "You forgot your Amphora password",
         "text": "You forgot your Amphora password"
     }
