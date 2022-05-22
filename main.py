e765f830966ac474fa2a4c4ef5bb9d615daa8de5
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-05-22 18:05:55 +0300

    enabled cors

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from episodes import episodes_router
 from images import images_router
 from shows import shows_router
 from users import auth_backend, UserDB, fastapi_users, current_active_user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
