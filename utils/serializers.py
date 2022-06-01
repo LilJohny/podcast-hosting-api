fa0006ac000a24011b8d91afe640918bd6bdfa72
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-01 19:24:23 +0300

    Refactoring

--- utils/serializers.py
+++ utils/serializers.py
@@ -3,5 +3,7 @@ from typing import Type, Union, List
 from sqlmodel import SQLModel
 
 
-def serialize(obj: Union[SQLModel, List[SQLModel]], serializer_class: Type[SQLModel], many: bool = False) -> list:
+def serialize(obj: Union[SQLModel, List[SQLModel]],
+              serializer_class: Type[SQLModel],
+              many: bool = False) -> Union[list, SQLModel]:
     return [serializer_class(**item.dict()) for item in obj] if many else serializer_class(**obj.dict())
