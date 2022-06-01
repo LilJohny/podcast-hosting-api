6bdfb995ff5f2231d3e9a3ded12a004d1c3bca25
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-01 16:08:39 +0300

    Typing fix

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,7 +1,7 @@
-from typing import Type, Union
+from typing import Type, Union, List
 
 from sqlmodel import SQLModel
 
 
-def serialize(obj: Union[SQLModel, list[SQLModel]], serializer_class: Type[SQLModel], many: bool = False) -> list:
+def serialize(obj: Union[SQLModel, List[SQLModel]], serializer_class: Type[SQLModel], many: bool = False) -> list:
     return [serializer_class(**item.dict()) for item in obj] if many else serializer_class(**obj.dict())
