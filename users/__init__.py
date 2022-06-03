5dc88b02a4804eaae18dd89b017f5b605a6ae9e8
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-04 01:52:19 +0300

    Refactoring

--- users/__init__.py
+++ users/__init__.py
@@ -1,2 +1,2 @@
 from users.db import User, UserAdmin
-from users.manager import current_active_user, fastapi_users, auth_backend
+from users.config import current_active_user, fastapi_users, auth_backend
---
+++ users/config.py
@@ -0,0 +1,25 @@
+import uuid
+
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
+
+from settings import USER_MANAGER_SECRET
+from users.db import User
+from users.manager import get_user_manager
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=USER_MANAGER_SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
+
+current_active_user = fastapi_users.current_user(active=True)
