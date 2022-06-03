14a17ecc9d7da388b31fb2b445cff99bf8910874
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-03 19:52:54 +0300

    Added fetching files, reimplemented uploading, added xml filekind

--- utils/files.py
+++ utils/files.py
@@ -1,12 +1,15 @@
 import enum
 import os
 
+import httpx
+
 from settings import aws_session, BUCKET_NAME, PUBLIC_URL
 
 
 class FileKind(enum.Enum):
     IMAGE = "public/images"
     AUDIO = "public/audios"
+    XML = "public/xml"
 
 
 def get_s3_key(file_name: str, title: str) -> str:
@@ -15,13 +18,18 @@ def get_s3_key(file_name: str, title: str) -> str:
     return s3_key
 
 
-async def upload_file_to_s3(file_name: str, file_title: str, file_data, file_kind: FileKind) -> str:
-    file_key = get_s3_key(file_name, file_title)
+async def upload_file_to_s3(file_key: str, file_data, file_kind: FileKind) -> str:
     blob_s3_key = f"{file_kind.value}/{file_key}"
-    async with aws_session.client("s3") as s3:
+    async with aws_session.resource("s3") as s3:
         try:
-            response = await s3.upload_fileobj(file_data, BUCKET_NAME, blob_s3_key)
-            print(response)
+            stored_object = await s3.Object(BUCKET_NAME, blob_s3_key)
+            await stored_object.put(Body=file_data)
         except Exception as err:
             print(err)
     return f"{PUBLIC_URL}/{blob_s3_key}"
+
+
+async def fetch_file(url):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+    return resp.content
