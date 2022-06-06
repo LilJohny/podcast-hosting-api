c0a2fb9db8f0f87d7ac6ebeae594ef39aea4be74
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-06 16:54:47 +0300

    Fixes

--- series/admin.py
+++ series/admin.py
@@ -3,5 +3,5 @@ from sqladmin import ModelAdmin
 from series.models import Series
 
 
-class SeriesAdmin(ModelAdmin, table=Series):
+class SeriesAdmin(ModelAdmin, model=Series):
     column_list = [Series.show_id, Series.name]
--- series/models.py
+++ series/models.py
@@ -3,9 +3,8 @@ import uuid
 from sqlmodel import Field
 
 from models import UUIDModel
-from shows.models import Show
 
 
 class Series(UUIDModel, table=True):
-    name: str = Field(primary_key=True, nullable=False)
-    show_id: uuid.UUID = Field(nullable=False, foreign_key=Show.id)
+    name: str = Field(nullable=False)
+    show_id: uuid.UUID = Field(nullable=False, foreign_key="show.id")
