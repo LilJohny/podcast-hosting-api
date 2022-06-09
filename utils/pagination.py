89536a0e0b9a163a658e29fb2886be07858f7ec8
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-09 14:18:08 +0300

    Refactoring

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,16 +1,13 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 
 from fastapi_pagination import resolve_params
 from fastapi_pagination.bases import AbstractParams, AbstractPage
 from fastapi_pagination.ext.sqlalchemy import paginate_query
 from sqlalchemy import func, select
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import Select
-
-
 
 
 async def paginate(
@@ -18,7 +15,6 @@ async def paginate(
     query: Select,
     params: Optional[AbstractParams] = None,
 ) -> AbstractPage:  # pragma: no cover # FIXME: fix coverage report generation
-    #print(params)
     params = resolve_params(params)
 
     total = await session.scalar(select(func.count()).select_from(query.subquery()))  # type: ignore
