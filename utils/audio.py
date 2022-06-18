2b245efedb6fc71ccc7a8902ece3d724aebb580e
Denys Ivanenko <ivanenko.denys01@gmail.com>
2022-06-18 07:02:54 +0300

    Added ogg support

--- utils/audio.py
+++ utils/audio.py
@@ -1,5 +1,6 @@
 from mutagen.mp3 import MP3
 from mutagen.wave import WAVE
+from mutagen.ogg import OggFileType
 
 
 def get_duration_mp3(audio_file):
@@ -10,8 +11,13 @@ def get_duration_wave(audio_file):
     return WAVE(audio_file).info.length
 
 
+def get_duration_ogg(audio_file):
+    return OggFileType(audio_file).info.length
+
+
 DURATION_FINDERS = {
     ".mp3": get_duration_mp3,
     ".wave": get_duration_wave,
-    ".wav": get_duration_wave
+    ".wav": get_duration_wave,
+    ".ogg": get_duration_ogg
 }
